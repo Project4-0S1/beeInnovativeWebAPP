d85ccf44a7e951fa319e6899caf6f0785ad07662
Jorrit Geurts <r0956231@student.thomasmore.be>
2025-01-14 09:07:53 +0100

    eerste deel NavBarKlaar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  constructor(private router: Router){}
+
   pages = [
     {path: '/', name: 'Home'},
     {path: '/Kaart', name: 'Kaart'},
