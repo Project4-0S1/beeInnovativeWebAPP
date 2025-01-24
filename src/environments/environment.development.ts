85fc4a20d8eb97c7f00fccadc49d56afd451db25
Jorrit Geurts <r0956231@student.thomasmore.be>
2025-01-24 15:21:44 +0100

    deze

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,10 +1,10 @@
 export const environment = {
-    production: true, // this is true in the production environment
+    production: false, // this is true in the production environment
     AUTH0_DOMAIN: "bee-innovative.eu.auth0.com",
     AUTH0_CLIENT_ID: "WkK0g7HcIxfGwRiEhNV0JNw2sP35Uroz",
     AUTH0_AUDIENCE: "https://beeinnovative.azurewebsites.net/",
-    redirectUri: "https://beeinnovative.netlify.app/callback",
-    api_url: "https://beeinnovative.azurewebsites.net/api/",
+    redirectUri: "http://localhost:4200/callback",
+    api_url: "https://localhost:7099/api/",
     mapbox: {
         accessToken: 'pk.eyJ1Ijoiam9ycml0LWdldXJ0cyIsImEiOiJjbTV3YjQybngwN2xxMmxzYXJ4bXI2dDRmIn0.2H-0nL81JkwycB_b9En0Bw' 
     }
