c53ceeda209ee00954ae174ee36c7a63d719b4e5
Marnick Goossens <77274498+MarnickGoossens@users.noreply.github.com>
2025-02-10 09:34:28 +0100

    link working

---
+++ src/app/detections/detections.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detections',
+  standalone: true,
+  imports: [],
+  templateUrl: './detections.component.html',
+  styleUrl: './detections.component.css'
+})
+export class DetectionsComponent {
+
+}
